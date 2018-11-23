1801979b895a2ff795fc6c7901f6d422cb850619
Chris Dimitrov <ChrisDimitrov95@gmail.com>
2018-11-23 16:24:35 +0200

    Initial commit.
    New VS project.

---
+++ QuickNet.Console/Program.cs
@@ -0,0 +1,22 @@
+﻿using QuickNet.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickNet.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VariableInteger vi = new VariableInteger();
+            vi.Convert(494878333);
+
+            var a = 2 << 62;
+            string binary = Convert.ToString(3L << 30, 2);
+            var res = (2U << 30) | (2642361981U >> 2);
+        }
+    }
+}
---
+++ QuickNet.Utilities/VariableInteger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickNet.Utilities
+{
+    public class VariableInteger
+    {
+        private int _size;
+        private byte[] _bytes;
+        public Int64 Integer { get; set; }
+
+        public UInt64 Convert(UInt64 integer)
+        {
+            // Test
+            UInt64 res = 0;
+            if (integer < 64)
+            {
+                res = (integer >> 2);
+            }
+            else if (integer < 16383)
+            {
+                res = (1 << 14) | (integer >> 2);
+            }
+            else if (integer < 1073741823)
+            {
+                res = (2UL << 30) | (integer >> 2);
+            }
+
+            return res;
+        }
+    }
+}
