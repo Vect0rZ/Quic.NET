1b43afc3260de83b0b48db1acb9bb990087fd237
Chris Dimitrov <ChrisDimitrov95@gmail.com>
2018-11-24 00:32:32 +0200

    Added Variable Integer.

--- QuickNet.Console/Program.cs
+++ QuickNet.Console/Program.cs
@@ -11,12 +11,9 @@ namespace QuickNet.Console
     {
         static void Main(string[] args)
         {
-            VariableInteger vi = new VariableInteger();
-            vi.Convert(494878333);
-
-            var a = 2 << 62;
-            string binary = Convert.ToString(3L << 30, 2);
-            var res = (2U << 30) | (2642361981U >> 2);
+            byte[] bytes = new VariableInteger(15293);
+            VariableInteger integer = bytes;
+            UInt64 uinteger = integer;
         }
     }
 }
--- QuickNet.Utilities/VariableInteger.cs
+++ QuickNet.Utilities/VariableInteger.cs
@@ -8,26 +8,59 @@ namespace QuickNet.Utilities
 {
     public class VariableInteger
     {
-        private int _size;
-        private byte[] _bytes;
-        public Int64 Integer { get; set; }
-
-        public UInt64 Convert(UInt64 integer)
+        private UInt64 _integer;
+        public VariableInteger(UInt64 integer)
         {
-            // Test
-            UInt64 res = 0;
-            if (integer < 64)
-            {
-                res = (integer >> 2);
-            }
-            else if (integer < 16383)
-            {
-                res = (1 << 14) | (integer >> 2);
-            }
-            else if (integer < 1073741823)
-            {
-                res = (2UL << 30) | (integer >> 2);
-            }
+            _integer = integer;
+        }
+
+        public static implicit operator byte[](VariableInteger integer)
+        {
+            return Encode(integer._integer);
+        }
+
+        public static implicit operator VariableInteger(byte[] bytes)
+        {
+            return new VariableInteger(Decode(bytes));
+        }
+
+        public static implicit operator UInt64(VariableInteger integer)
+        {
+            return integer._integer;
+        }
+
+        public static byte[] Encode(UInt64 integer)
+        {
+            int requredBytes = 0;
+            if (integer <= 63)
+                requredBytes = 1;
+            else if (integer <= 16383)
+                requredBytes = 2;
+            else if (integer <= 1073741823)
+                requredBytes = 4;
+            else if (integer <= 4611686018427387903)
+                requredBytes = 8;
+
+            byte[] uInt64Bytes = BitConverter.GetBytes(integer);
+            byte last = uInt64Bytes[requredBytes - 1];
+            last = (byte)(last | (requredBytes / 2) << 6);
+            uInt64Bytes[requredBytes - 1] = last;
+
+            byte[] result = new byte[requredBytes];
+            Buffer.BlockCopy(uInt64Bytes, 0, result, 0, requredBytes);
+
+            return result;
+        }
+
+        public static UInt64 Decode(byte[] bytes)
+        {
+            int i = bytes.Length - 1;
+            byte[] buffer = new byte[8];
+
+            Buffer.BlockCopy(bytes, 0, buffer, 0, bytes.Length);
+            buffer[i] = (byte)(buffer[i] & (255 >> 2));
+
+            UInt64 res = BitConverter.ToUInt64(buffer, 0);
 
             return res;
         }
