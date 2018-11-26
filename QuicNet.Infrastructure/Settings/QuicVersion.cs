38341e4185416d8b43b8bf8774c8a3d95c8c820e
Chris Dimitrov <ChrisDimitrov95@gmail.com>
2018-11-26 15:27:15 +0200

    Added Quic Version constants.

---
+++ QuicNet.Infrastructure/Settings/QuicVersion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuicNet.Infrastructure.Settings
+{
+    public class QuicVersion
+    {
+        public const int CurrentVersion = 16;
+
+        public static readonly List<int> SupportedVersions = new List<int>() { 15, 16 };
+    }
+}
