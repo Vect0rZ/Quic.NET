13327d21498c833c261825fdf3b863f64dc55e65
Chris Dimitrov <ChrisDimitrov95@gmail.com>
2018-12-07 22:49:40 +0200

    Added UdpSession.

---
+++ QuicNet/Context/UdpSession.cs
@@ -0,0 +1,40 @@
+﻿using QuicNet.Infrastructure.Packets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuicNet.Context
+{
+    /// <summary>
+    /// Wrapper of the UdpClient to be referenced in the Connections/Streams.
+    /// </summary>
+    public class UdpSession
+    {
+        private UdpClient _client;
+        private IPEndPoint _endpoint;
+
+        public UdpSession(UdpClient client, IPEndPoint endpoint)
+        {
+            _client = client;
+            _endpoint = endpoint;
+        }
+
+        public bool Send(Packet packet)
+        {
+            byte[] data = packet.Encode();
+            // Ignore empty packets
+            if (data == null || data.Length <= 0)
+                return true;
+
+            int result = _client.Send(data, data.Length, _endpoint);
+            if (result <= 0)
+                return false;
+
+            return true;
+        }
+    }
+}
