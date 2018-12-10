44092fd428d9ed41cb7ca9135a6e2c254d4f5bfd
Chris Dimitrov <ChrisDimitrov95@gmail.com>
2018-12-10 23:19:42 +0200

    Added number space.

---
+++ QuicNet.Infrastructure/NumberSpace.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuicNet.Infrastructure
+{
+    public class NumberSpace
+    {
+        private UInt32 _max = UInt32.MaxValue;
+        private UInt32 _n = 1;
+
+        public NumberSpace()
+        {
+        }
+
+        public NumberSpace(UInt32 max)
+        {
+            _max = max;
+        }
+
+        public UInt32 Get()
+        {
+            if (_n >= _max)
+                return 0;
+
+            _n++;
+            return _n;
+        }
+    }
+}
