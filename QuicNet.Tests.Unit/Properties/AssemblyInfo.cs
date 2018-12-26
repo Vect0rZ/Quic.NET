73bb7930ea75f2331604c14f2c9b63a62233c7a8
Chris Dimitrov <ChrisDimitrov95@gmail.com>
2018-12-27 00:19:05 +0200

    Added VariableInteger unit tests.

---
+++ QuicNet.Tests.Unit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("QuicNet.Tests.Unit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("QuicNet.Tests.Unit")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4170c9d8-b2d1-4e59-9f5f-c9665cad9a37")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ QuicNet.Tests.Unit/VariableIntegerTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QuickNet.Utilities;
+
+namespace QuicNet.Tests.Unit
+{
+    [TestClass]
+    public class VariableIntegerTests
+    {
+        [TestMethod]
+        public void Zero()
+        {
+            VariableInteger integer = new VariableInteger(0);
+            byte[] bin = integer;
+            UInt64 num = integer;
+
+            Assert.IsNotNull(bin);
+            Assert.AreEqual(bin.Length, 1);
+            Assert.AreEqual(bin[0], (byte)0);
+            Assert.AreEqual(num, (UInt64)0);
+        }
+
+        [TestMethod]
+        public void One()
+        {
+            VariableInteger integer = new VariableInteger(1);
+            byte[] bin = integer;
+            UInt64 num = integer;
+
+            Assert.IsNotNull(bin);
+            Assert.AreEqual(bin.Length, 1);
+            Assert.AreEqual(bin[0], (byte)1);
+            Assert.AreEqual(num, (UInt64)1);
+        }
+
+        [TestMethod]
+        public void Test63()
+        {
+            VariableInteger integer = new VariableInteger(63);
+            byte[] bin = integer;
+            UInt64 num = integer;
+
+            Assert.IsNotNull(bin);
+            Assert.AreEqual(bin.Length, 1);
+            Assert.AreEqual(bin[0], (byte)63);
+            Assert.AreEqual(num, (UInt64)63);
+        }
+
+        [TestMethod]
+        public void Test64()
+        {
+            VariableInteger integer = new VariableInteger(64);
+            byte[] bin = integer;
+            UInt64 num = integer;
+
+            Assert.IsNotNull(bin);
+            Assert.AreEqual(bin.Length, 2);
+            Assert.AreEqual(bin[0], (byte)64);
+            Assert.AreEqual(bin[1], (byte)64);
+            Assert.AreEqual(num, (UInt64)64);
+        }
+
+        [TestMethod]
+        public void Test256()
+        {
+            VariableInteger integer = new VariableInteger(256);
+            byte[] bin = integer;
+            UInt64 num = integer;
+
+            Assert.IsNotNull(bin);
+            Assert.AreEqual(bin.Length, 2);
+            Assert.AreEqual(bin[0], (byte)65);
+            Assert.AreEqual(bin[1], (byte)0);
+            Assert.AreEqual(num, (UInt64)256);
+        }
+    }
+}
