128afe9676841e3b7061c25e676bc598ac7dc044
Chris Dimitrov <ChrisDimitrov95@gmail.com>
2018-12-28 19:55:08 +0200

    Initial QuicClient

--- QuicNet/QuicClient.cs
+++ QuicNet/QuicClient.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using QuicNet.Context;
+using QuicNet.Infrastructure.PacketProcessing;
+using QuicNet.Infrastructure.Packets;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +13,34 @@ namespace QuicNet
 {
     public class QuicClient
     {
+        private IPEndPoint _peerIp;
+        private UdpClient _client;
 
+        private Unpacker _unpacker;
+        private InitialPacketCreator _packetCreator;
+
+        public QuicContext Context { get; private set; }
+
+        public QuicClient()
+        {
+
+        }
+
+        public bool Connect(string ip, int port)
+        {
+            // Establish socket connection
+            _peerIp = new IPEndPoint(IPAddress.Parse(ip), port);
+            _client.Connect(_peerIp);
+
+            // Create quic context
+            Context = new QuicContext(_client, _peerIp);
+
+            // Start initial protocol process
+            InitialPacket connectionPacket = _packetCreator.CreateInitialPacket(0, 0);
+            if (!Context.Send(connectionPacket))
+                return false;
+
+            return true;
+        }
     }
 }
