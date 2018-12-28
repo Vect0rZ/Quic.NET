d724d8d46a5094445e843e68e04bc0d515bdbf94
Chris Dimitrov <ChrisDimitrov95@gmail.com>
2018-12-28 20:22:12 +0200

    Added console project for client

---
+++ QuicNet.Tests.ConsoleClient/Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuicNet.Tests.ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuicClient client = new QuicClient();
+        }
+    }
+}
--- QuickNet.Tests.ConsoleServer/Program.cs
+++ QuickNet.Tests.ConsoleServer/Program.cs
@@ -11,7 +11,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace QuickNet.Console
+namespace QuickNet.Tests.ConsoleServer
 {
     class Program
     {
