ebf387dbc8c07caa1615cd9ceee9c635f8e812be
Chris Dimitrov <ChrisDimitrov95@gmail.com>
2018-12-28 20:53:59 +0200

    Added functionality to connect client to server

---
+++ QuicNet/Exceptions/QuicConnectivityException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuicNet.Exceptions
+{
+    public class QuicConnectivityException : Exception
+    {
+        public QuicConnectivityException(string message) : base(message)
+        {
+        }
+    }
+}
--- QuicNet/QuicClient.cs
+++ QuicNet/QuicClient.cs
@@ -1,6 +1,10 @@
-﻿using QuicNet.Context;
+﻿using QuicNet.Connections;
+using QuicNet.Context;
+using QuicNet.Exceptions;
+using QuicNet.Infrastructure.Frames;
 using QuicNet.Infrastructure.PacketProcessing;
 using QuicNet.Infrastructure.Packets;
+using QuicNet.Infrastructure.Settings;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,32 +20,82 @@ namespace QuicNet
         private IPEndPoint _peerIp;
         private UdpClient _client;
 
+        private QuicConnection _connection;
+
         private Unpacker _unpacker;
         private InitialPacketCreator _packetCreator;
 
-        public QuicContext Context { get; private set; }
+        private UInt64 _maximumStreams = QuicSettings.MaximumStreamId;
 
         public QuicClient()
         {
             _client = new UdpClient();
+            _unpacker = new Unpacker();
             _packetCreator = new InitialPacketCreator();
         }
 
-        public bool Connect(string ip, int port)
+        public QuicContext Connect(string ip, int port)
         {
             // Establish socket connection
             _peerIp = new IPEndPoint(IPAddress.Parse(ip), port);
             _client.Connect(_peerIp);
 
-            // Create quic context
-            Context = new QuicContext(_client, _peerIp);
-
             // Start initial protocol process
             InitialPacket connectionPacket = _packetCreator.CreateInitialPacket(0, 0);
-            if (!Context.Send(connectionPacket))
-                return false;
+            byte[] data = connectionPacket.Encode();
 
-            return true;
+            // Send the initial packet
+            _client.Send(data, data.Length);
+
+            // Await response for sucessfull connection creation by the server
+            byte[] peerData = _client.Receive(ref _peerIp);
+            if (peerData == null)
+                throw new QuicConnectivityException("Server did not respond properly.");
+
+            Packet packet = _unpacker.Unpack(peerData);
+            if ((packet is InitialPacket) == false)
+                throw new QuicConnectivityException("Server did not respond properly.");
+
+            InitialPacket ini = (InitialPacket)packet;
+
+            HandleInitialFrames(packet);
+            EstablishConnection(ini.DestinationConnectionId, ini.SourceConnectionId);
+
+            // Create the QuicContext
+            QuicContext context = new QuicContext(_client, _peerIp);
+            context.Connection = this._connection;
+
+            return context;
+        }
+
+        private void HandleInitialFrames(Packet packet)
+        {
+            List<Frame> frames = packet.GetFrames();
+            for (int i = frames.Count - 1; i > 0; i--)
+            {
+                Frame frame = frames[i];
+                if (frame is ConnectionCloseFrame)
+                {
+                    ConnectionCloseFrame ccf = (ConnectionCloseFrame)frame;
+
+                    throw new QuicConnectivityException(ccf.ReasonPhrase);
+                }
+
+                if (frame is MaxStreamsFrame)
+                {
+                    MaxStreamsFrame msf = (MaxStreamsFrame)frame;
+                    _maximumStreams = msf.MaximumStreams.Value;
+                }
+
+                // Break out if the first Padding Frame has been reached
+                if (frame is PaddingFrame)
+                    break;
+            }
+        }
+
+        private void EstablishConnection(UInt32 connectionId, UInt32 peerConnectionId)
+        {
+            _connection = new QuicConnection(connectionId, peerConnectionId);
         }
     }
 }
